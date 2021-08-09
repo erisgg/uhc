c71f1df8ad175514cb7f519a1f1ee3d1ecfaa27a
alfiejfs <alfie@alfiesmith.net>
2021-08-09 16:38:51 +0100

    Fix for new permission

--- uhc-custom-craft/src/main/java/gg/eris/uhc/customcraft/game/CustomCraftUhcIdentifiers.java
+++ uhc-custom-craft/src/main/java/gg/eris/uhc/customcraft/game/CustomCraftUhcIdentifiers.java
@@ -2,13 +2,12 @@ package gg.eris.uhc.customcraft.game;
 
 import gg.eris.commons.bukkit.ErisBukkitCommons;
 import gg.eris.commons.bukkit.permission.Permission;
+import gg.eris.commons.bukkit.permission.PermissionGroup;
 import gg.eris.commons.bukkit.permission.PermissionRegistry;
 import gg.eris.commons.core.identifier.Identifier;
 import gg.eris.commons.core.identifier.IdentifierProvider;
-import javax.naming.directory.InvalidAttributeIdentifierException;
 import lombok.experimental.UtilityClass;
 import org.bukkit.Bukkit;
-import org.checkerframework.checker.index.qual.PolyUpperBound;
 
 @UtilityClass
 public final class CustomCraftUhcIdentifiers {
@@ -32,16 +31,19 @@ public final class CustomCraftUhcIdentifiers {
   public static final String GAME_WORLD = "world";
   public static final String GAME_NETHER = "world_nether";
 
-  public static final Identifier VIEWSPECTATORS_PERMISSION = permission("viewspectators");
+  public static final Identifier VIEWSPECTATORS_PERMISSION = permission(
+      "viewspectators",
+      PermissionGroup.STAFF
+  );
 
-  private static Identifier permission(String name) {
+  private static Identifier permission(String name, PermissionGroup group) {
     PermissionRegistry registry =
         Bukkit.getServicesManager()
             .getRegistration(ErisBukkitCommons.class)
             .getProvider()
             .getPermissionRegistry();
 
-    return Permission.ofDefault(registry, name).getIdentifier();
+    return Permission.ofDefault(registry, name, group).getIdentifier();
   }
 
 }
