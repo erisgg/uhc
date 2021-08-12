364fbd33e7ae0740f05e0fd571ee57c9029a309e
alfiejfs <alfie@alfiesmith.net>
2021-08-12 05:55:37 +0100

    bullshit excuse

--- uhc-custom-craft/src/main/java/gg/eris/uhc/customcraft/craft/menu/ultimate/UltimateShopItem.java
+++ uhc-custom-craft/src/main/java/gg/eris/uhc/customcraft/craft/menu/ultimate/UltimateShopItem.java
@@ -3,6 +3,10 @@ package gg.eris.uhc.customcraft.craft.menu.ultimate;
 import gg.eris.commons.bukkit.menu.Menu;
 import gg.eris.commons.bukkit.menu.MenuItem;
 import gg.eris.commons.bukkit.menu.MenuViewer;
+import gg.eris.commons.bukkit.text.TextController;
+import gg.eris.commons.bukkit.text.TextType;
+import gg.eris.commons.bukkit.util.CC;
+import gg.eris.commons.bukkit.util.ItemBuilder;
 import gg.eris.uhc.customcraft.craft.ultimate.UltimateCraft;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -15,11 +19,16 @@ public class UltimateShopItem implements MenuItem {
 
   @Override
   public ItemStack getItem(MenuViewer viewer, Menu menu) {
-    return this.ultimate.getItem();
+    return new ItemBuilder(this.ultimate.getItem())
+        .addLore("", CC.RED.bold() + "LOCKED").build();
   }
 
   @Override
   public void onClick(MenuViewer viewer, InventoryClickEvent event) {
-
+    TextController.send(
+        viewer.getPlayer(),
+        TextType.ERROR,
+        "Ultimates are currently <h>locked</h> at this stage of the BETA to perfect regular balance!"
+    );
   }
 }
