5f9f83007e6fdd41a42365c7a1e2735433e22a00
alfiejfs <alfie@alfiesmith.net>
2021-08-14 10:54:22 +0100

    tick inventory back

--- uhc-custom-craft/src/main/java/gg/eris/uhc/customcraft/craft/TrinketListener.java
+++ uhc-custom-craft/src/main/java/gg/eris/uhc/customcraft/craft/TrinketListener.java
@@ -1,15 +1,22 @@
 package gg.eris.uhc.customcraft.craft;
 
 import gg.eris.commons.core.identifier.Identifier;
+import gg.eris.uhc.core.game.UhcGame;
 import gg.eris.uhc.core.game.state.GameState;
 import gg.eris.uhc.core.game.state.listener.type.GameStateListener;
 import gg.eris.uhc.customcraft.craft.vocation.Trinket;
 import gg.eris.uhc.customcraft.craft.vocation.Vocation;
 import gg.eris.uhc.customcraft.craft.vocation.VocationUnlockable;
+import gg.eris.uhc.customcraft.game.CustomCraftUhcGame;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+@RequiredArgsConstructor
 public final class TrinketListener extends GameStateListener {
+
+  private final CustomCraftUhcGame game;
 
   @Override
   protected void onEnable(GameState<?, ?> state) {
@@ -27,6 +34,8 @@ public final class TrinketListener extends GameStateListener {
     VocationUnlockable newUnlockable = Vocation.getUnlockable(identifier);
     if (newUnlockable instanceof Trinket) {
       event.setCancelled(true);
+      Bukkit.getScheduler().runTaskLater(this.game.getPlugin(),
+          () -> event.getPlayer().updateInventory(), 2L);
     }
   }
 
