f1d733d8682cb085062934031ef2ce81e2c7ac34
alfiejfs <alfie@alfiesmith.net>
2021-08-16 05:29:19 +0100

    fix spec

--- uhc-custom-craft/src/main/java/gg/eris/uhc/customcraft/command/SpectateCommand.java
+++ uhc-custom-craft/src/main/java/gg/eris/uhc/customcraft/command/SpectateCommand.java
@@ -9,20 +9,12 @@ import gg.eris.uhc.core.game.state.GameState.Type;
 import gg.eris.uhc.core.game.state.GameState.TypeRegistry;
 import gg.eris.uhc.customcraft.CustomCraftUhcIdentifiers;
 import gg.eris.uhc.customcraft.game.CustomCraftUhcGame;
-import java.util.Set;
-import javax.naming.CompositeName;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class SpectateCommand implements CommandProvider {
 
   private final CustomCraftUhcGame game;
-
-  private static final Set<Type> IN_GAME = Set.of(
-      TypeRegistry.GRACE_PERIOD,
-      TypeRegistry.PVP,
-      TypeRegistry.DEATHMATCH
-  );
 
   @Override
   public Builder getCommand(CommandManager manager) {
@@ -41,7 +33,8 @@ public class SpectateCommand implements CommandProvider {
             "You are already a spectator."
         );
         return;
-      } else if (type == TypeRegistry.ENDED || type == TypeRegistry.STARTING) {
+      } else if (type != TypeRegistry.GRACE_PERIOD && type != TypeRegistry.PVP
+          && type != TypeRegistry.DEATHMATCH) {
         TextController.send(
             context.getSenderAsPlayer(),
             TextType.ERROR,
@@ -50,13 +43,7 @@ public class SpectateCommand implements CommandProvider {
         return;
       }
 
-
-
-      if (type == TypeRegistry.WAITING || type == TypeRegistry.COUNTDOWN) {
-        this.game.removePlayer(this.game.getPlayer(context.getSenderAsPlayer()));
-      } else {
-        this.game.killPlayer(this.game.getPlayer(context.getSenderAsPlayer()), null, null);
-      }
+      this.game.killPlayer(this.game.getPlayer(context.getSenderAsPlayer()), null, null);
 
       TextController.send(
           context.getSenderAsPlayer(),
